95606db0e7f9dd128c5125f281b7346ad9679ac8
Kuldeep Bhagat <kuldeep496242@gmail.com>
2026-03-16 21:10:13 +0530

    layout added

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,24 @@ export default function App() {
                             border border-white/10
                             shadow-2xl
                             rounded-xl
-                            my-auto
-                            overflow-hidden">
-                <h1 className="text-white p-2 font-light">Calculator</h1>
+                            flex
+                            flex-row
+                            ">
+                <div className="bg-red-500 flex-1">
+                    <h1 className="text-white p-2 font-light">Calculator</h1>
+                    <div className="bg-amber-950">
+                        <h1>options</h1>
+                    </div>
+                    <div className="bg-green-200">
+                        <h1>result</h1>
+                    </div>
+                    <div className="bg-orange-600">
+                        <h1>keys</h1>
+                    </div>
+                </div>
+                <div className="bg-yellow-500 flex-1">
+                    <h1 className="text-white p-2 font-light">Right side</h1>
+                </div>
             </div>
         </div>
     )
