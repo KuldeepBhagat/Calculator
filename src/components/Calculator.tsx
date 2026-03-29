bc686325c81692aea6651de11eb394b94221435c
Kuldeep Bhagat <kuldeep496242@gmail.com>
2026-03-30 00:31:35 +0530

    calculator logic 2.0

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react'
 
 export default function Calculator() {
 
-    interface Operations {
+    interface HistoryType {
         num1: string,
         operator: string,
         num2: string,
@@ -17,35 +17,51 @@ export default function Calculator() {
     const [CurrentNumber, setCurrentNumber] = useState<Array<string>>(["0"])
     const [Record, setRecord] = useState<Array<string>>([])
     const [waitingState, setWaitingState] = useState<boolean[]>([false, false])
-    const [operationPipeline, setOperationPipeline] = useState<Operations[]>([])
+    const [operationPipeline, setOperationPipeline] = useState({
+        num1: "",
+        num2: "",
+        operator: ""
+    })
+    const [historyPipeline, setHistory] = useState<HistoryType[]>([])
 
     const operands: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
     const operators: string[] = ["+", "−", "÷", "×"]
     const specialOperators: string[] = ["%", "¹/ₓ", "x²", "√x"]
     const ControlButtons: string[] = ["⌫", "C", "CE"]
 
-    console.log(operationPipeline)
-    
     function Calculate(key: string): number {
-        const num1 = parseFloat(operationPipeline.at(-1)?.num1!)
-        const operator = operationPipeline.at(-1)?.operator
-        const num2 = parseFloat(CurrentNumber.join(''))
+
+
+        let num1 = parseFloat(operationPipeline.num1)
+        const operator = operationPipeline.operator
+        let num2 = parseFloat(CurrentNumber.join(''))
         let result: number;
 
-        switch(operator) {
+        if (Record.at(-1) === "=") {
+            num1 = parseFloat(CurrentNumber.join(''))
+            num2 = parseFloat(operationPipeline.num2)
+        }
+
+        switch (operator) {
             case "+": result = num1 + num2
-                      break;
+                break;
             case "−": result = num1 - num2
-                      break
+                break
             case "×": result = num1 * num2
-                      break;
+                break;
             case "÷": result = num1 / num2
-                      break
+                break
             default: result = 0
         }
 
-        return result
+        setHistory(prev => [...prev, {
+            num1: `${num1}`,
+            operator: operator,
+            num2: `${num2}`,
+            result: `${result}`
+        }])
 
+        return result
     }
 
     function calculate(key: string): void {
@@ -78,6 +94,12 @@ export default function Calculator() {
             if (waitingState[0]) {
                 setCurrentNumber([])
             }
+
+            if (Record.at(-1) === "=") {
+                setRecord([])
+                setCurrentNumber([])
+            }
+
             setCurrentNumber(prev => [...prev, key])
             setWaitingState([false, false])
         }
@@ -88,27 +110,20 @@ export default function Calculator() {
             if (operators.includes(Record.at(-1)!)) {
                 setRecord(prev => [...prev.slice(0, -1), key])
 
-                if (waitingState[1] && operationPipeline.at(-1)?.operator) {
-                    setOperationPipeline(prev => {
-                        const lastIndex = prev.length - 1;
-                        return prev.map((val, index) => {
-                            if (index === lastIndex) {
-                                return { ...val, operator: key }
-                            }
-                            return val;
-                        })
-                    })
+                if (waitingState[1] && operationPipeline.operator) {
+                    setOperationPipeline(prev => ({
+                        ...prev,
+                        operator: key
+                    }))
                 }
-
             } else {
                 const CompleteNumber = CurrentNumber.join('');
                 setRecord([CompleteNumber, key])
-                setOperationPipeline(prev => [...prev, {
+                setOperationPipeline(prev => ({
+                    ...prev,
                     num1: CompleteNumber,
                     operator: key,
-                    num2: "",
-                    result: ""
-                }])
+                }))
             }
 
             if (!waitingState[1] && Record.length > 1) {
@@ -116,30 +131,36 @@ export default function Calculator() {
                 const result = Calculate(key)
                 const num2 = parseFloat(CurrentNumber.join(''))
 
+                
                 setRecord([`${result}`, key])
                 setCurrentNumber([`${result}`])
-                setOperationPipeline(prev => {
-                    const length = operationPipeline.length - 1
-                    return prev.map((val, index) => {
-                        if(index === length) {
-                            return {...val, num1: `${result}`, operator: key, num2: `${num2}`}
-                        }
-                        return val
-                    })
-                })
+                setOperationPipeline(prev => ({ ...prev, num1: `${result}`, operator: key, num2: `${num2}` }))
             }
 
-            if(!waitingState[1] && Record.length > 1 && key === "=") {
-                let result = Calculate(key)
+        }
 
+        if (Record.length > 1 && key === "=") {
+            // this code can be sorted but im too lazy to do so
+            const operator = Record.at(1)
+            if (Record.at(-1) === "=") {
+                setRecord(prev => prev.map((val, index) => index === 0 ? CurrentNumber.join('') : val))
+                const result = Calculate(operator!)
+                setCurrentNumber([`${result}`])
+                setOperationPipeline(prev => ({ ...prev, num1: `${result}` }))
+            } else {
+                const result = Calculate(operator!)
+                const num2 = parseFloat(CurrentNumber.join(''))
+                setRecord(prev => [...prev, `${num2}`, "="])
+                setCurrentNumber([`${result}`])
+                setOperationPipeline(prev => ({ ...prev, num1: `${result}`, operator: operator!, num2: `${num2}` }))
             }
         }
     }
 
     return (
         <div className="max-w-4xl
-                        min-h-9/10 
-                        w-full
+                        h-[90vh]
+                        w-[95%]
                         backdrop-blur-xs
                         bg-white/5
                         border border-white/10
@@ -149,6 +170,7 @@ export default function Calculator() {
                         flex-row
                         z-20
                         relative
+                        overflow-hidden
                         ">
             <div className="flex flex-col flex-2 text-white m-1">
                 <Header />
@@ -156,8 +178,8 @@ export default function Calculator() {
                 <MemoryButton />
                 <Keypad calculate={calculate} />
             </div>
-            <div className="flex-1 flex flex-col">
-                <HistoryAndMemory />
+            <div className="flex-1 flex flex-col min-h-0">
+                <HistoryAndMemory historyPipeline={historyPipeline} />
             </div>
         </div>
     )
--- src/components/HistoryAndMem.tsx
+++ src/components/HistoryAndMem.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react"
 
-export default function HistoryAndMemory() {
+type operation = {
+        num1: string,
+        operator: string,
+        num2: string,
+        result: string
+    }
+
+type props = {
+    historyPipeline: operation[]
+}
+
+export default function HistoryAndMemory({historyPipeline}: props) {
     const buttonStyle = "p-2 hover:text-white/50"
 
     const [activeTab, setActiveTab] = useState<string>("History");
     return (
-        <>
+        <div className="flex flex-col h-full min-h-0">
             <div className="items-center flex-row p-2 gap-2 text-white">
                 <button className={buttonStyle} onClick={(() => setActiveTab("History"))}>History</button>
                 <button className={buttonStyle} onClick={() => setActiveTab("Memory")}>Memory</button>
@@ -18,16 +29,30 @@ export default function HistoryAndMemory() {
                                 "translate-x-19"}`}>
                 </div>
             </div>
-            <div className={`flex-1 p-3 text-white ${activeTab === "Memory" ?
-                                                       "hidden" : null
-            }`}>
-                <h1>this is the History</h1>
+            <div className={`flex-1 min-h-0  p-3
+                           text-white flex 
+                             flex-col overflow-y-auto
+                             [&::-webkit-scrollbar]:w-1.5
+                             [&::-webkit-scrollbar]:bg-transparent
+                             [&::-webkit-scrollbar-thumb]:bg-white/10
+                             [&::-webkit-scrollbar-thumb]:rounded-full
+                             hover:[&::-webkit-scrollbar-thumb]:bg-white/20
+                             
+             ${activeTab === "Memory" ?
+                                                      "hidden" : ""}`}>
+                {historyPipeline.map((m, index) => {
+                    return (
+                        <button key={index} className="p-3 shrink-0 flex justify-end text-white/40 hover:bg-white/10 cursor-pointer rounded-sm">
+                            {m.num1} {m.operator} {m.num2} = {m.result}
+                        </button>
+                    )
+                })}
             </div>
             <div className={`flex-1 p-3 text-white ${activeTab === "History" ?
-                                                       "hidden" : null
+                                                       "hidden" : ""
             }`}>
-                <h1>this is the Memory</h1>
+                <h1></h1>
             </div>
-        </>
+        </div>
     )
 }
